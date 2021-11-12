9aff876105a7db8435a6f20e7a335239d42991c9
Michael Knutson <mrknutson44@gmail.com>
2021-11-12 08:44:39 -0900

    added github workflow comments

--- demo.rb
+++ demo.rb
@@ -3,3 +3,13 @@ puts "hello world"
 
 
 puts "1 + 1 = 2"
+
+# git setup
+# git init to create a repo
+# create a repo on gitHub
+# add the remote link
+
+# make changes in your code
+# add(stage) changes to the working tree git add .
+# commit those changes git commit -m'description of changes'
+# push up to gitHub git push origin main
